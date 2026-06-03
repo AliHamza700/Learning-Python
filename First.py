1f2888292062dad234cda52f5a1d4a32d5c58929
AliHamza700 <alihamza67865>
2026-06-03 17:07:52 +0500

    Python File

---
+++ First.py
@@ -0,0 +1,21 @@
+# Multiple Lines Print in the Format /Multiple Lines print in the print function using triple quotes
+# print("Python is the core language — you need it deeply. Not just scripting; real OOP with inheritance, decorators, and class design, because every Odoo model is a Python class. XML in Odoo is not like HTML — it defines the UI structure of every screen (forms, lists, kanban boards) and is something you write every single day. QWeb is Odoo's templating language for generating PDF reports and web pages; think of it as Python's Jinja2 but Odoo-specific. JavaScript is needed at a moderate level — you use it to build or extend frontend widgets using OWL, Odoo's own component framework (very similar conceptually to React). SQL is used at a medium level — the ORM handles most of it, but you need it to read query plans and debug performance issues.","Hello ","Hello02")
+# Print the table
+# 5*1=0
+# 5*1=5
+# 5*2=10
+# num*i
+#result=0 , 5+10=15,20
+# num=10
+
+# for i in range(1,num+1):
+#     result=num*i
+#     print(f"{num}*{i}={result}")
+import pyttsx3
+engine = pyttsx3.init()
+
+# For Mac, If you face error related to "pyobjc" when running the `init()` method :
+# Install 9.0.1 version of pyobjc : "pip install pyobjc>=9.0.1"
+
+engine.say("""""")
+engine.runAndWait()
