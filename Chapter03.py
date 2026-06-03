3c16d402b7fcba07c001b23cd4059e2e2a6af135
AliHamza700 <alihamza67865>
2026-06-03 19:25:41 +0500

    String slicing

--- Chapter03.py
+++ Chapter03.py
@@ -1,4 +1,4 @@
-Name="Ali"#Double quotes
+Name="Hamza"#Double quotes
 # Name='ALi'#single quotes
 # Name=""""ALi"""#Triple quotes
 # print(Name) #strings are immutable in python ,which cannot be changed
@@ -6,5 +6,7 @@ Name="Ali"#Double quotes
 # print(Name[0]) #A
 # for normal staring of the string ,start from the 0 and in the case of negative indexing start from the -1
 # print(Name[-1]) #i
-shortname=Name[0:2]#slicing of the string
-print(shortname) #AL
+# shortname=Name[0:2]#slicing of the string
+# print(shortname) #Ha
+# negtive slicing
+print(Name[-3:-1])#slicing of the string
