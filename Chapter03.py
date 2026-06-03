39076b871a1b4e990bf5f9571b1734ff9d08576b
AliHamza700 <alihamza67865>
2026-06-03 18:28:28 +0500

    Learning the Slicing of the String

---
+++ Chapter03.py
@@ -0,0 +1,10 @@
+Name="Ali"#Double quotes
+# Name='ALi'#single quotes
+# Name=""""ALi"""#Triple quotes
+# print(Name) #strings are immutable in python ,which cannot be changed
+# strings are ordered sequence of characters, which can be accessed by index
+# print(Name[0]) #A
+# for normal staring of the string ,start from the 0 and in the case of negative indexing start from the -1
+# print(Name[-1]) #i
+shortname=Name[0:2]#slicing of the string
+print(shortname) #AL
