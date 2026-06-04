714e2833e52a83a446ff845ffa6dca77775d83f4
AliHamza700 <alihamza67865>
2026-06-04 18:06:26 +0500

    String Quiz

---
+++ Chapter03/Quiz.py
@@ -0,0 +1,26 @@
+#Print the each character of the word
+Name="Hamza"
+#we have two options Manually and using the loop #
+# let done by both ways
+# print(Name[0])#H
+# print(Name[1])#A
+# print(Name[2])#M
+# print(Name[3])#Z
+# print(Name[4])#A
+# using the loop 
+# for i in Name :
+#  print(i)
+ #Problem no 02 Count the Character in the string
+#  Counts="Pakistan"
+#  print(Counts.count("a"))
+#This method only count the  occurance of the chartacer not how many we have total 
+# print(len(Counts))# we have use the len method to count the total number of the character  
+# problem no 03 and print the first and last charctdr of the string 
+First_Last="Pakistan"
+# print(First_Last[0])  # Print the first character
+# print(First_Last[-1])  # Print the last character
+# Reverse the string 
+# Reverse="Reverse"
+#  print(Reverse[::,-1])
+# Count the vowels in the string
+text = "education"
---
+++ Chapter03/tempCodeRunnerFile.py
@@ -0,0 +1,1 @@
+print(Reverse[::,-1])
